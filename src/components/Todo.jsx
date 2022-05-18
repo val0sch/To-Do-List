e1ac9075a88daaae038222ab8f4be57cd76ace8f
Valerie <valerie.ielsch@gmail.com>
2022-05-18 21:27:19 +0200

    bouton delete fonctionnel , bouton completed renvoit true or false, avec un style spécifique (la tâche est rayée si completed :true)

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 
-const Todo = ({ text }) => {
+const Todo = ({ todo, todos, setTodos }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : null}`}>
+        {todo.text}
+      </li>
+      <button className="complete-btn" onClick={completeHandler}>
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button className="trash-btn" onClick={deleteHandler}>
         <i className="fas fa-trash"></i>
       </button>
     </div>
