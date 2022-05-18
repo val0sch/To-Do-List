be93c3c645b3b4a6f35dffce2f7825a52eb3bc45
Valerie <valerie.ielsch@gmail.com>
2022-05-18 17:46:44 +0200

    test add h4 for deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-  <div>
-  <h2>Github Pages</h2>
-  <h3>Deploying React to Github</h3>
-  </div>
+    <div>
+      <h2>Github Pages</h2>
+      <h3>Deploying React to Github</h3>
+      <h4>hello world</h4>
+    </div>
   );
- }
+}
 
 export default App;
