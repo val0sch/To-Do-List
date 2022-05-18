58fee0bc5e61ffa64172558fbfbad88b86fccc6f
Valerie <valerie.ielsch@gmail.com>
2022-05-18 20:35:22 +0200

    bouton submit fonctionnel , les tâches insérées dans l'input s'ajoutent à une liste

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Valerie's to do list</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }
